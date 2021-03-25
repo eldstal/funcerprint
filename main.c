00e2b755c498a9f0abffc9163431905d46890a04
eldstal <laeder.keps@gmail.com>
2021-03-25 21:35:48 +0100

    More code. Oh yes.

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <string.h>
 
 int fib(int n) {
   if (n < 2) return 1;
   return fib(n-1) + fib(n-2);
 }
 
-size_t _strlen(char* text) {
+size_t _strlen(const char* text) {
   size_t len = 0;
   for (len=0; *text!=0; len++) text++;
   return len;
@@ -26,8 +27,24 @@ uint64_t byteswap(uint64_t in) {
   return out;
 }
 
+void xor_encode(char* data, size_t data_len, const char* key, size_t key_len) {
+  for (size_t i=0; i<data_len; i++) {
+    data[i] ^= key[i%key_len];
+  }
+}
+
 
 int main() {
+
+  char text[20] = {0};
+  const char* key = "leetsp33k";
+  strncpy(text, "Yo ho ho and a bottle fo rum!", 19);
+
+  xor_encode(text, _strlen(text), key, _strlen(key));
+
+  uint64_t feedbeef = byteswap(0xEFBEEDFE00000000LL);
+  feedbeef = _strlen(text);
+  feedbeef = fib(feedbeef);
 
   return 0;
 }
